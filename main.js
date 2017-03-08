9b52ed5ccdf93fbd7b1abcbe6d35b9800ba44724
tythursby <Hal@Tylers-Mac-Pro.local>
2017-03-07 20:05:17 -0500

    ready for styling

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-$(document).ready(function(){
-  var long;
+$(document).ready(function loca(){
+
+  var tempF;
+  var tempC;
+  var city;
+  var zip;
+  var country;
+  var lon;
   var lat;
-  var temp;
+  var state;
 
-var api = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e5e62079a2fcfa4112afa14c181597e1";
+var api_1 = "http://ip-api.com/json";
+  $.getJSON(api_1, function vars(obj) {
+  console.log(obj);
+  zip = obj.zip;
+  city = obj.city;
+  country = obj.countryCode;
+  lon = obj.lon;
+  lat = obj.lat;
+  state = obj.regionName;
 
+
+var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=e5e62079a2fcfa4112afa14c181597e1";
   $.getJSON(api, function(data){
-    alert("Your current location is " + data.coord.lat +" "+ data.coord.lon);
+
+    var kel = data.main.temp;
+    tempF = Math.round(1.8*(kel - 273) + 32);
+    tempC = Math.round((5/9)*(tempF - 32));
+
+    $("#tempC").html(tempC);
+    $("#temp").html(tempF + "&deg;F");
+    $("#loc").html(city +"<br>" +state);
+
+    console.log(api);
+    console.log(api_1);
+    console.log(city);
+    console.log(zip);
+    console.log(country);
+    console.log(tempF);
+    console.log(lon);
+    console.log(lat);
+    console.log(state);
+    });
   });
+  var toggleC = false;
+
+  $("#cToF").on("click", function() {
+      if (toggleC == true) {
+        toggleC = false;
+        $("#temp").html(tempF + "&deg;F");
+      } else if (toggleC == false) {
+        toggleC = true;
+        $("#temp").html(tempC + "&deg;C");
+      }
+    });
 });
